3a37706687411f95b39e5c3cee4931ef2595f856
Lâm Nguyễn Bửu Duy <67887468+Lamduy0807@users.noreply.github.com>
2023-10-09 21:38:14 +0700

    add (#6)

---
+++ src/Pages/Components/Grid/index.js
@@ -0,0 +1,62 @@
+import React from "react";
+import "./grid.css";
+const Grid = (props) => {
+  //   const style = {
+  //     gridRowGap: props.rowGap ? `${props.rowGap}px` : "0",
+  //     gridColGap: props.colGap ? `${props.colGap}px` : "0",
+  //   };
+
+  return (
+    <div className="grid">
+      
+      {/* <div className="col-3 col-s-12">
+        <div>
+          <img
+            style={{ height: "100px" }}
+            src="https://e1.pxfuel.com/desktop-wallpaper/379/959/desktop-wallpaper-beautiful-love-for-full-screen-beautiful.jpg"
+          />
+        </div>
+        <div>
+          <img
+            style={{ height: "100px" }}
+            src="https://e1.pxfuel.com/desktop-wallpaper/379/959/desktop-wallpaper-beautiful-love-for-full-screen-beautiful.jpg"
+          />
+        </div>
+        <div>
+          <img
+            style={{ height: "100px" }}
+            src="https://e1.pxfuel.com/desktop-wallpaper/379/959/desktop-wallpaper-beautiful-love-for-full-screen-beautiful.jpg"
+          />
+        </div>
+        <div className="col-4 col-s-12">
+          <div>
+            <img
+              style={{ height: "100px" }}
+              src="https://e1.pxfuel.com/desktop-wallpaper/379/959/desktop-wallpaper-beautiful-love-for-full-screen-beautiful.jpg"
+            />
+          </div>
+          <div>
+            <img
+              style={{ height: "100px" }}
+              src="https://e1.pxfuel.com/desktop-wallpaper/379/959/desktop-wallpaper-beautiful-love-for-full-screen-beautiful.jpg"
+            />
+          </div>
+          <div style={{ height: "100px" }}>
+            <img
+              style={{ height: "100px" }}
+              src="https://e1.pxfuel.com/desktop-wallpaper/379/959/desktop-wallpaper-beautiful-love-for-full-screen-beautiful.jpg"
+            />
+          </div>
+          <div>
+            <img
+              style={{ height: "100px" }}
+              src="https://e1.pxfuel.com/desktop-wallpaper/379/959/desktop-wallpaper-beautiful-love-for-full-screen-beautiful.jpg"
+            />
+          </div>
+        </div>
+      </div> */}
+    </div>
+  );
+};
+
+export default Grid;
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,27 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import Grid from "../Components/Grid";
+import axios from "axios";
+import { useMutation } from "react-query";
 /**
  *
  * Redux: 3 thành phần
  * 1. Actions =>
+ * aysnc/ await => bất đồng bộ
+ * get => GET restful API
+ * post => POST
+ * create => PUT
  */
+const fetchingPosts = async () => {
+  const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+
+  return res.data;
+};
+
 const Home = () => {
-  const infors = useSelector((state) => state.infors);
-  console.log("🚀 ~ file: index.js:11 ~ Home ~ infors:", infors);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const mutation = useMutation((payload) => {
+    axios.post("https://jsonplaceholder.typicode.com/posts", payload);
+  });
+
+  if (mutation.isLoading) return <div>loading...</div>;
+
+  if (mutation.isError) return <div>{mutation.error.message}</div>;
+
+  if (mutation.isSuccess) return window.alert("Thành công!!");
 
   return (
     <div>
       <div>Home</div>
       <Link to={"contact"}>Link to contact</Link>
 
-      {infors.map((infor) => {
+      {/* {infors.map((infor) => {
         return (
           <div key={infor.id}>
             data: {infor.data}, time: {infor.time}
           </div>
         );
-      })}
+      })} */}
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+        placeholder="Enter title"
+      />
+
+      <input
+        type="text"
+        value={body}
+        onChange={(e) => {
+          setBody(e.target.value);
+        }}
+        placeholder="Enter body"
+      />
+
+      <button
+        onClick={() => {
+          mutation.mutate({ title, body });
+        }}
+      >
+        submit
+      </button>
     </div>
   );
 };
